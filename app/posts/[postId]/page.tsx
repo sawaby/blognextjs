d9aff65d218a8cb32a14556db02f4fc491a7d541
Maria <sawaby@users.noreply.github.com>
2023-11-05 16:07:59 -0800

    minor change

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,57 +1,57 @@
 import getFormattedDate from "@/lib/getFormattedDate"
-import { getPostData, getSortedPostsData } from "@/lib/posts"
-import { notFound } from 'next/navigation'
+import { getSortedPostsData, getPostData } from "@/lib/posts"
+import { notFound } from "next/navigation"
 import Link from "next/link"
 
-export function generateStaticParams(){
-    const posts = getSortedPostsData();
+export function generateStaticParams() {
+    const posts = getSortedPostsData()
+
     return posts.map((post) => ({
         postId: post.id
     }))
 }
 
-export function generateMetaData({ params }: {params: { postId: string }}) {
+export function generateMetadata({ params }: { params: { postId: string } }) {
 
     const posts = getSortedPostsData()
     const { postId } = params
 
-   const post = posts.find(post => post.id === postId)
-   if (!post){
-    return {
-        title: 'Post Not Found'
+    const post = posts.find(post => post.id === postId)
+
+    if (!post) {
+        return {
+            title: 'Post Not Found'
+        }
     }
-   }
-   return { 
-    title: post.title,
-   }
+
+    return {
+        title: post.title,
+    }
 }
 
-
-export default async function page({ params }: {params: { postId: string }}) {
+export default async function Post({ params }: { params: { postId: string } }) {
 
     const posts = getSortedPostsData()
     const { postId } = params
 
-    if(!posts.find( post => post.id === postId)) {
-        return notFound()
-    }
+    if (!posts.find(post => post.id === postId)) notFound()
 
-    const { title, date, contentHtml } = await getPostData(postId);
+    const { title, date, contentHtml } = await getPostData(postId)
 
-    const pubDate = getFormattedDate(date);
-  return (
-    
-    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-    <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-    <p className="mt-0">
-        {pubDate}
-    </p>
-    <article>
-        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-        <p>
-            <Link href="/">← Back to home</Link>
-        </p>
-    </article>
-</main>
-  )
+    const pubDate = getFormattedDate(date)
+
+    return (
+        <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+            <p className="mt-0">
+                {pubDate}
+            </p>
+            <article>
+                <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+                <p>
+                    <Link href="/">← Back to home</Link>
+                </p>
+            </article>
+        </main>
+    )
 }
